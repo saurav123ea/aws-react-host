409d3ccd43f4d4ba4444fbbfb7295c1603f13415
LAPTOP-DM7LL34D <sauravvinod16@gmail.com>
2022-09-20 08:21:25 +0530

    first commit

---
+++ src/constants/endpoints.js
@@ -0,0 +1,12 @@
+export const BASE_URI ='http://sauravtweetapp-env-1.eba-65wzp3bd.ap-northeast-1.elasticbeanstalk.com'
+export const BASE_TWEET_URL = '/api/v1.0/tweets'
+export const FORGOT_PASSWD = '/forgetPassword'
+export const AUTHENTICATE = '/authenticate'
+export const LOGOUT = '/logout/' 
+export const REGISTER = '/register' 
+export const ALL_TWEETS = '/all'
+export const ALL_USERS = '/users/all'
+export const GET_USER = "/api/v1.0/user/search/"
+
+
+
